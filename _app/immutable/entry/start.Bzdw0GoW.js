b6786c1862e816f16b3f07c95bdbf26a4e125860
Alwin Nedrum <74955192+HEYROCKME@users.noreply.github.com>
2026-03-14 22:32:52 +0100

    Delete _app directory

--- _app/immutable/entry/start.Bzdw0GoW.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/ixuJq1vy.js";import{x as t}from"../chunks/C2bbA3o0.js";export{t as load_css,r as start};
--- _app/immutable/entry/start.CwZ14tWb.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CIjlzEAr.js";import{x as t}from"../chunks/8AOAOAyZ.js";export{t as load_css,r as start};
