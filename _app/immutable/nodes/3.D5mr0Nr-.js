f9f39720723bc7e4dbb568725ff69ac4202b55ec
Alwin Nedrum <74955192+HEYROCKME@users.noreply.github.com>
2026-03-16 10:05:18 +0100

    Updated with clickable meny, and routes

---
+++ _app/immutable/nodes/3.D5mr0Nr-.js
@@ -0,0 +1,9 @@
+import{a as m,f as i}from"../chunks/Cm6LlBSI.js";import"../chunks/BJ0TOJAT.js";import{g as d,i as t,j as g,k as p,l as f,n as c,q as k}from"../chunks/CzK1Lja0.js";import{F as u}from"../chunks/fxeooFyz.js";import{M as v}from"../chunks/MrkrGs8f.js";var b=i(`<header class="lg:pl-32 lg:pr-32 p-2 m-2 flex justify-between align-baseline"><h1 class="mt-4 ekkometoden mb-16 font-black svelte-cwls5q">ekkometoden</h1> <!></header> <main class="svelte-cwls5q"><section class=" sm:w-md md:w-lg  mt-8 z-10 p-4 mb-16 order-5 svelte-cwls5q"><h1>om oss</h1> <h2 class="text-3xl mb-4 font-black">ekkometoden</h2> <p class="font-medium text-2xl">Vi er en nord-norsk gründerbedrift startet i mars 2026.
+            Gjennom <span class="font-black ekko svelte-cwls5q">ekko</span> gir vi fagpersoner
+            verktøy for å følge opp ungdommer med milde til moderate rusutfordringer.</p> <p class="mt-16 bio svelte-cwls5q"><span class="font-black svelte-cwls5q">Randulf Nedrum</span> er utdannet
+            idretts- og spesialpedagog med videreutdanning i atferdsvansker, tilrettelegging
+            for barn med funksjonsnedsettelser og emosjonelle vansker, samtalemetodikk
+            og pedagogisk veiledning. Han har lang erfaring med oppfølging av barn,
+            ungdom og familier i komplekse situasjoner. Gjennom mange år har han
+            vært opptatt av at ungdom med rusutfordringer skal få et forsvarlig og
+            forutsigbart tilbud.</p></section></main> <!>`,1);function _(r){let s=k(!0);var a=b(),e=d(a),o=t(g(e),2);v(o,{get open(){return f(s)},set open(l){p(s,l)},$$legacy:!0}),c(e);var n=t(e,4);u(n),m(r,a)}export{_ as component};
